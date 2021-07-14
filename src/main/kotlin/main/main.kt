838f54118c4f0ed7aa16c11684b753fa70204e1e
mrivh <m.alicia.riv@gmail.com>
2021-07-14 02:07:51 -0500

    Added final elements due to connection errors
    Co-authored-by: RandyVillanueva1 <2520160004rvillanuevam@gmail.com>
    Co-authored-by: HeyGera <geraldine.mtts@gmail.com>
    Co-authored-by: AcesTerra <jovengf@gmail.com>

--- src/main/kotlin/main/main.kt
+++ src/main/kotlin/main/main.kt
@@ -1,8 +1,12 @@
 package main
 
-import sesion3.*
+import sesion1_2_3.Login
+import sesion1_2_3.Market
+import sesion1_2_3.Whishlist
+import sesion5_6.Car
 
-fun main(){
+
+fun main() {
     /*var userCntr = 0
     var productCntr = 0
     val buyer1 = Buyer(userCntr,"Alfredo", "alfredo@gmail.com","alf123")
@@ -12,9 +16,12 @@ fun main(){
     val amazoon = Amazoon()
     amazoon.registerBuyer(buyer1)*/
 
-    val log = Login(user = String(),  pass = String(), us = String(), pas = String())
-    val market = Market(nameproduct = String(),typeproduct = String(),brand = String(),color = String(),
-        manyproducts = String(),description = String())
+    val log = Login(user = String(), pass = String(), us = String(), pas = String())
+    val market = Market(
+        nameproduct = String(), typeproduct = String(), brand = String(), color = String(),
+        manyproducts = String(), description = String())
+    val whishlist = Whishlist()
+    val car = Car(product = String(), precio = Int.MAX_VALUE , cantidad = Int.MAX_VALUE, id_compra = Int.MAX_VALUE)
 
     println("[         Amazoon          ]")
 
@@ -29,39 +36,71 @@ fun main(){
     log.pas = readLine().toString()
 
     if (log.user == log.us && log.pass == log.pas) {
-        println("Hola ${log.user} ¡Bienvenido a tu súper cuenta de Amazoon :) !")
+        println("Hola ${log.user} ¡Bienvenido a tu súper cuenta de Amazoon :D !")
     } else {
         println("Error al encontrar al usuario!!")
     }
-    if(log.user == log.us && log.pass == log.pas) {
-        println("---------Ingreso de productos------------")
 
-        println ("Ingresa el nombre de tu producto")
-        market . nameproduct = readLine ().toString()
+    println("Ingresa '1' si quieres ingresar productos que quieras vender.")
+    println("Ingresa '2' si quieres visualizar los productos de la tienda y poder comprar.")
+    val option: Int = readLine()!!.toInt()
 
-        println ("Ingresa la categoria de tu producto")
-        market . typeproduct = readLine ().toString()
+    var options = when (option) {
+        1 -> if (log.user == log.us && log.pass == log.pas) {
+            println("---------Ingreso de productos------------")
 
-        println ("Ingresa la marca de tu producto")
-        market . brand = readLine ().toString()
+            println("Ingresa el nombre de tu producto")
+            market.nameproduct = readLine().toString()
 
-        println ("Ingresa el color de tu producto")
-        market . color = readLine ().toString()
+            println("Ingresa la categoria de tu producto")
+            market.typeproduct = readLine().toString()
 
-        println ("Ingresa la cantidad de unidadades de tu producto")
-        market . manyproducts = readLine ().toString()
+            println("Ingresa la marca de tu producto")
+            market.brand = readLine().toString()
 
-        println ("Ingresa una descripcion breve de tu producto")
-        market . description = readLine ().toString()
+            println("Ingresa el color de tu producto")
+            market.color = readLine().toString()
 
-        println ("producto agregado exitosamente")
+            println("Ingresa la cantidad de unidadades de tu producto")
+            market.manyproducts = readLine().toString()
 
-        println (
-            "Nombre del producto ${market.nameproduct} de la categoria ${market.typeproduct} " +
-                    "de la marca ${market.brand} de color ${market.color} con la cantidad de ${market.manyproducts} ${market.description}"
+            println("Ingresa una descripcion breve de tu producto")
+            market.description = readLine().toString()
+
+            println("producto agregado exitosamente")
+
+            println(
+                "Nombre del producto ${market.nameproduct} de la categoria ${market.typeproduct} " +
+                        "de la marca ${market.brand} de color ${market.color} con la cantidad de ${market.manyproducts} ${market.description}"
+            )
+        } else {
+            println("Acceso a tu cuenta denegado!!")
+        }
+        2 -> println(
+            "${whishlist.printyourproducts()}" +
+                    "${whishlist.printallproducts()}"
         )
-    }else{
-
-        println("Acceso a tu cuenta denegado!!")
+        else -> println("Solo puedes seleccionar la option 1 o 2")
     }
+    println("----Ingresa el numero de la categoria del tipo tipo de productos a comprar----- ")
+    println(" '1' Ropa")
+    println(" '2' Joyeria")
+    println(" '3' Juguetes")
+    println(" '4' Electronica")
+    println(" '5' Cosas del hogar")
+    println(" '6' Electrodomesticos")
+    val option2: Int = readLine()!!.toInt()
+    var options2 = when (option2) {
+        1 -> println("${car.cate1()} ")
+        2 -> println("${car.cate2()} ")
+        3 -> println("${car.cate3()} ")
+        4 -> println("${car.cate4()}")
+        5 -> println("${car.cate5()} ")
+        6 -> println("${car.cate6()}")
+        else ->
+            println("Solo puedes ingresar de la opción 1 a la 6 ")
+    }
+
 }
+
+
